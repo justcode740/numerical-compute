464ed41b455b5cc1b9fc3c6249845abda35863f9
0x0 <coollightning123@gmail.com>
2023-09-06 16:00:45 +0800

    cleanup

--- examples/matrix.rs
+++ examples/matrix.rs
@@ -2,9 +2,7 @@ use std::env::{var, set_var};
 use clap::Parser;
 use halo2_scaffold::scaffold::cmd::Cli;
 use halo2_scaffold::scaffold::run_builder_on_inputs;
-use nalgebra::{ Matrix2};
-
-use halo2_base::utils::{ScalarField};
+use halo2_base::utils::ScalarField;
 use halo2_base::AssignedValue;
 #[allow(unused_imports)]
 use halo2_base::{
@@ -30,7 +28,8 @@ fn some_algorithm_in_zk<F: ScalarField>(
     input: CircuitInput,
     make_public: &mut Vec<AssignedValue<F>>,
 ) {
-    
+    let m = input.matrix.len();
+    let n = input.matrix[0].len();
 
     let lookup_bits =
         var("LOOKUP_BITS").unwrap_or_else(|_| panic!("LOOKUP_BITS not set")).parse().unwrap();
@@ -52,15 +51,13 @@ fn some_algorithm_in_zk<F: ScalarField>(
             let target = ctx.load_witness(target);
             let diff = fixed_point_chip.qsub(ctx, val, target);
             let abs_diff = fixed_point_chip.qabs(ctx, diff);
-            print!("diff val: {:?}", fixed_point_chip.dequantization(*diff.value()));
             // let diff_square = fixed_point_chip.qmul(ctx, diff, diff);
             // print!("diff sqaure val: {:?}", fixed_point_chip.dequantization(*diff_square.value()));
             acc = fixed_point_chip.qadd(ctx, acc, abs_diff);
-            print!("acc val: {:?}", fixed_point_chip.dequantization(*acc.value()));
         }
     }
-    
-    let err = fixed_point_chip.qsqrt(ctx, acc);
+    let matrix_size = ctx.load_constant(fixed_point_chip.quantization((m * n) as f64));
+    let err = fixed_point_chip.qdiv(ctx, acc, matrix_size);
     let err_decimal = fixed_point_chip.dequantization(*err.value());
     println!("err: {:?}", err_decimal);
     assert!(err_decimal < input.err); 
@@ -88,8 +85,6 @@ fn naive_matrix_mul(u: &Vec<Vec<f64>>, s: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     result
 }
 
-
-
 fn main() {
     env_logger::init();
     set_var("LOOKUP_BITS", 12.to_string());
@@ -100,45 +95,6 @@ fn main() {
         vec![3.0,4.0]
     ];
     
-    let matrix2 = Matrix2::from_vec(
-        matrix.concat()
-    );
-
-    // Perform SVD
-    let svd = matrix2.svd(true, true);
-  
-    // Convert U to Vec<Vec<f64>>
-    let u_vec: Vec<Vec<f64>> = vec![
-        vec![svd.u.unwrap().m11, svd.u.unwrap().m12],
-        vec![svd.u.unwrap().m21, svd.u.unwrap().m22]
-    ];
-
-    let s = vec![svd.singular_values[0], svd.singular_values[1]];
-    let mut eigen_matrix: Vec<Vec<f64>> = vec![vec![0.0; 2]; 2];
-
-    // Fill the diagonal elements with values from the vector
-    for i in 0..2 {
-        eigen_matrix[i][i] = s[i];
-    }
-
-    // Convert V^T to Vec<Vec<f64>>
-    let v_t_vec: Vec<Vec<f64>> = vec![
-        vec![svd.v_t.unwrap().m11, svd.v_t.unwrap().m12],
-        vec![svd.v_t.unwrap().m21, svd.v_t.unwrap().m22]
-    ];
-
-    u_vec.iter().for_each(|row| {
-        println!("uvec: {}", row.iter().map(|&e| e.to_string()).collect::<Vec<_>>().join(" "));
-    });
-
-    eigen_matrix.iter().for_each(|row| {
-        println!("eigen: {}", row.iter().map(|&e| e.to_string()).collect::<Vec<_>>().join(" "));
-    });
-
-    v_t_vec.iter().for_each(|row| {
-        println!("vt: {}", row.iter().map(|&e| e.to_string()).collect::<Vec<_>>().join(" "));
-    });
-
     let args = Cli::parse();
 
     let m1: Vec<Vec<f64>> = vec![
@@ -156,7 +112,7 @@ fn main() {
         vec![0.81741556, -0.57604844],
     ];
 
-    let input = CircuitInput { u: m1, s: m2, vt: m3, matrix, err: 0.5  };
+    let input = CircuitInput { u: m1, s: m2, vt: m3, matrix, err: 1e-5 };
     // run different zk commands based on the command line arguments
     run_builder_on_inputs(
         |builder, input, public| some_algorithm_in_zk(builder.main(0), input, public),
